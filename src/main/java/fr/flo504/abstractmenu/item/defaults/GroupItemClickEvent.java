3419162c20762766d4ce8f82a1ed3c77d0b5f3bb
Flo504 <Flo504.Dev@gmail.com>
2020-08-15 19:01:24 +0200

    Add method to link ToggleItem from a group

--- src/main/java/fr/flo504/abstractmenu/item/defaults/GroupItemClickEvent.java
+++ src/main/java/fr/flo504/abstractmenu/item/defaults/GroupItemClickEvent.java
@@ -5,10 +5,7 @@ import fr.flo504.abstractmenu.item.ToggleItem;
 import org.bukkit.entity.Player;
 import org.bukkit.event.inventory.ClickType;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Objects;
+import java.util.*;
 import java.util.stream.Collectors;
 
 
@@ -32,6 +29,10 @@ public class GroupItemClickEvent implements ItemClickEvent{
         this.other.addAll(Arrays.stream(other).filter(Objects::nonNull).collect(Collectors.toList()));
     }
 
+    public final void registerOther(Collection<ToggleItem> other){
+        this.other.addAll(other.stream().filter(Objects::nonNull).collect(Collectors.toList()));
+    }
+
     @Override
     public void onClick(ClickType type, Player player) {
         for(ToggleItem item : other){
@@ -43,4 +44,17 @@ public class GroupItemClickEvent implements ItemClickEvent{
 
         item.setMustToggleOnClick(false);
     }
+
+    public static void linkOther(ToggleItem... toggleItems){
+        for(ToggleItem item : toggleItems){
+            if(!(item.getEvent() instanceof GroupItemClickEvent))
+                continue;
+            ((GroupItemClickEvent) item.getEvent()).registerOther(
+                    Arrays.stream(toggleItems)
+                            .filter(toggleItem -> toggleItem != item)
+                            .collect(Collectors.toList())
+            );
+        }
+    }
+
 }

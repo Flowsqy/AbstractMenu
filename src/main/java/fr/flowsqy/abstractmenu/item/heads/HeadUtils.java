d60cb3b1d350bdebece6795691ac395a58225389
Flowsqy <47575244+Flowsqy@users.noreply.github.com>
2025-02-23 22:53:47 +0100

    Adapt HeadUtils to 1.21 and onwards

--- src/main/java/fr/flowsqy/abstractmenu/item/heads/HeadUtils.java
+++ src/main/java/fr/flowsqy/abstractmenu/item/heads/HeadUtils.java
@@ -1,35 +1,44 @@
 package fr.flowsqy.abstractmenu.item.heads;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Objects;
+import java.util.UUID;
+
 import org.bukkit.Bukkit;
 import org.bukkit.inventory.meta.SkullMeta;
 
-import java.lang.reflect.Field;
-import java.util.Objects;
-import java.util.UUID;
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
 
 public class HeadUtils {
 
     private static final Field profileField;
+    private static final Constructor<?> profileConstructor;
     private static final UUID genericUUID;
 
     static {
+        genericUUID = new UUID(0L, 0L);
         try {
-            final Class<?> cSkullMetaClass = Class.forName(Bukkit.getServer().getClass().getPackage().getName() + ".inventory.CraftMetaSkull");
+            final Class<?> cSkullMetaClass = Class
+                    .forName(Bukkit.getServer().getClass().getPackage().getName() + ".inventory.CraftMetaSkull");
             profileField = cSkullMetaClass.getDeclaredField("profile");
             profileField.setAccessible(true);
-            genericUUID = new UUID(0L, 0L);
-        } catch (ClassNotFoundException | NoSuchFieldException e) {
-            throw new RuntimeException();
+            final Class<?> nmsResolvableProfileClass = Class
+                    .forName("net.minecraft.world.item.component.ResolvableProfile");
+            profileConstructor = nmsResolvableProfileClass.getDeclaredConstructor(GameProfile.class);
+            profileConstructor.setAccessible(true);
+        } catch (ClassNotFoundException | NoSuchFieldException | NoSuchMethodException e) {
+            throw new RuntimeException(e);
         }
     }
 
     public static void applyProfile(SkullMeta meta, GameProfile profile) {
         Objects.requireNonNull(meta);
         try {
-            profileField.set(meta, profile);
-        } catch (IllegalAccessException e) {
+            profileField.set(meta, profileConstructor.newInstance(profile));
+        } catch (IllegalAccessException | InvocationTargetException | InstantiationException e) {
             throw new RuntimeException(e);
         }
     }
